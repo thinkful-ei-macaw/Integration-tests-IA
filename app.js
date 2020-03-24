9a04fa9b8951c5b6ffdea8e0cc7f69b5383619d8
MediocreIan <idrews094@gmail.com>
2020-03-24 11:38:53 -0500

    completed exercise

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const morgan = require("morgan");
+const playStore = require("./playstore");
+
+const app = express();
+app.use(morgan("dev"));
+
+app.get("/app", (req, res) => {
+  let results = [...playStore];
+  const { sort, genres } = req.query;
+
+  if (sort) {
+    if (!(sort === "Rating") && !(sort === "App")) {
+      res.status(400).send("sort should either be by app or rating");
+    } else if (sort) {
+      results.sort((a, b) => {
+        return a[sort] < b[sort] ? 1 : a[sort] > b[sort] ? -1 : 0;
+      });
+    }
+  }
+  if (genres) {
+    const validGenres = [
+      "Action",
+      "Puzzle",
+      "Strategy",
+      "Casual",
+      "Arcade",
+      "Card"
+    ];
+    if (!validGenres.includes(genres)) {
+      res.status(400).send("genre must be one of the listed genres");
+    } else {
+      results = results.filter(app => {
+        return app["Genres"].includes(genres);
+      });
+    }
+  }
+  res.json(results);
+});
+
+app.listen(8000, () => {
+  console.log("llistening on 8000");
+});
